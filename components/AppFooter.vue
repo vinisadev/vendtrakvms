2f8585f563ced2bb5a4547e70eff1bcb0a1828b4
Vin Is A Dev <vinfehring@gmail.com>
2024-12-11 16:56:13 -0600

    Simplify everything to match supabase example and also downgrade supabase

---
+++ components/AppFooter.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h3>VendTrakVMS - Copyright 2024 - domain76, Inc. All rights reserved.</h3>
+  </div>
+</template>
---
+++ components/AppHeader.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+  console.log('AppHeader.vue - setup script');
+  const supabase = useSupabaseClient();
+  const user = useSupabaseUser();
+
+  async function signout() {
+    await supabase.auth.signOut();
+    navigateTo('/', { replace: true });
+  }
+</script>
+
+<template>
+  <div>
+    <h3>VendTrakVMS</h3>
+    <div v-if="user">Logged in as: {{ user.email }}: <button @click="signout()">Sign Out</button></div>
+    <div v-if="!user">Not logged in</div>
+    <hr>
+  </div>
+</template>
