c36d4a0751b32d07b0549ed4b02582a613c5d0a4
Vincenzo Fehring <vinfehring@gmail.com>
2025-04-14 01:14:58 -0500

    Add sendgrid, confirmation email sending, api route reference

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         <div className="min-h-screen pt-20 flex flex-col">
           {children}
         </div>
+        <Toaster />
       </body>
     </html>
   );
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,12 +1,28 @@
 import { betterAuth } from "better-auth"
 import { drizzleAdapter } from "better-auth/adapters/drizzle"
 import { db } from "@/db"
+import { sendEmail } from "@/actions/email"
+import { openAPI } from "better-auth/plugins"
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg"
   }),
   emailAndPassword: {
-    enabled: true
-  }
+    enabled: true,
+    requireEmailVerification: true,
+  },
+  emailVerification: {
+    sendOnSignUp: true,
+    autoSignInAfterVerification: true,
+    sendVerificationEmail: async ({ user, token }) => {
+      const verificationUrl = `${process.env.BETTER_AUTH_URL}/api/auth/verify-email?token=${token}&callbackURL=${process.env.EMAIL_VERIFICATION_CALLBACK_URL}`
+      await sendEmail({
+        to: user.email,
+        subject: "VendTrakVMS Email Verification",
+        text: `Click the link to very your email: ${verificationUrl}`
+      })
+    }
+  },
+  plugins: [openAPI()]
 })
